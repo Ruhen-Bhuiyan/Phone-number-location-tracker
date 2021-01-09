ca90e0b24df103ccdf76bd79103092b48ad9fe49
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-01-09 22:13:15 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from phonenumbers import carrier
 from phonenumbers import geocoder
 from phone_iso3166.country import phone_country
 
-
 class Track:
     def __init__(self, App):
         self.window = App
@@ -14,7 +13,7 @@ class Track:
         self.window.geometry("500x400")
         self.window.configure(bg="#2f2212")
         self.window.resizable(True, True)
-
+        
         Label(App, text="Enter the phone number", fg="orange", font=("Times", 20), bg="#3f5edb").place(x=150, y=30)
         self.mobile_number = Entry(App, width=14, font=("Arial", 15), relief="flat")
         self.trackingbutton = Button(App, text="To track the number click here", bg="#22c6c3", relief="sunken")
@@ -27,12 +26,10 @@ class Track:
         self.countryname2.place(x=300, y=280)
 
         self.trackingbutton.bind("<Button-1>", self.Track_THE_location)
-
+        
     def Track_THE_location(self, event):
         phone_number = self.mobile_number.get()
-
         if phone_number:
-
             tracked = phonenumbers.parse(phone_number, "RO")
             tracked2 =phonenumbers.parse(phone_number, "CH")
         self.countryname.configure(text=carrier.name_for_number(tracked,"en"))
