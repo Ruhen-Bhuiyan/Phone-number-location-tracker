9e3c92dabdee0ba614e051fbdd4dbf88a6fdd177
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-05-26 03:52:25 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import phonenumbers
 import pycountry
 from tkinter import Tk, Label, Button, Entry
 from phonenumbers import carrier
+
 from phonenumbers import geocoder
 from phone_iso3166.country import phone_country
 class Track:
@@ -20,6 +21,7 @@ class Track:
         self.mobile_number.place(x=170, y=120)
         self.trackingbutton.place(x=200, y=200)
         self.countryname.place(x=100, y=280)
+        
         self.countryname2.place(x=300, y=280)
         self.trackingbutton.bind("<Button-1>", self.Track_THE_location)           
     def Track_THE_location(self, event):    
